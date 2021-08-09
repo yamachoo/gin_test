528edb81f0913153159bcf9286bc5d964204063c
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-09 23:22:22 +0900

    add AsciiJSON

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
 	return r
 }
 
--- main_test.go
+++ main_test.go
@@ -15,6 +15,17 @@ func TestPingRoute(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, http.StatusOK, w.Code)
 	assert.JSONEq(t, `{"message": "pong"}`, w.Body.String())
+}
+
+func TestSomeJSONRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/someJSON", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, `{"lang": "GO语言", "tag": "<br>"}`, w.Body.String())
 }
