2b21d70e3d42686f60d4c075fda0a9c7d936182d
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-10 15:29:58 +0900

    add Middleware

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 
 func main() {
 	engine := gin.Default()
+	ua := ""
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+			"message":    "hello world",
+			"User-Agent": ua,
 		})
 	})
 	engine.Run(":3000")
