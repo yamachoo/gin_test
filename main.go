59bd9344f335973056d829ae8fb9fd69504c4a90
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-10 15:25:22 +0900

    add hello world

--- main.go
+++ main.go
@@ -6,49 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var secrets = gin.H{
-	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
-	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
-	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
-}
-
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
+func main() {
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "hello world",
 		})
 	})
-
-	r.GET("/someJSON", func(c *gin.Context) {
-		data := map[string]interface{}{
-			"lang": "GO语言",
-			"tag":  "<br>",
-		}
-
-		c.AsciiJSON(http.StatusOK, data)
-	})
-
-	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
-		"foo":    "bar",
-		"austin": "1234",
-		"lena":   "hello2",
-		"manu":   "4321",
-	}))
-	authorized.GET("/secrets", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-		if secret, ok := secrets[user]; ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-		}
-	})
-
-	return r
-}
-
-func main() {
-	r := setupRouter()
-	r.Run()
+	engine.Run(":3000")
 }
