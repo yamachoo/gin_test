0c0b50940f59cf1b91e9d904838dafce47243bf9
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-10 15:06:56 +0900

    add BasicAuth

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -22,6 +28,21 @@ func setupRouter() *gin.Engine {
 		}
 
 		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+	authorized.GET("/secrets", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
 	})
 
 	return r
